dc6d55fcbcee47c70d37408d755fff2a7fcc2d93
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2021-07-02 20:00:52 +0600

    Add files via upload

---
+++ Number of Divisors using prime Factorization.cpp
@@ -0,0 +1,94 @@
+
+/*
+ Number of Divisors counting using prime factorization...
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+
+
+ll gcd ( ll a, ll b )
+{
+    return __gcd ( a, b );
+}
+ll lcm ( ll a, ll b )
+{
+    return a * ( b / gcd ( a, b ) );
+}
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+const int MX=1e6+123;
+bitset<MX>is_prime;
+vector<ll>primes;
+void sieve(ll n)
+{
+    ll sq=sqrt(n);
+    for(ll i=3; i<=n; i+=2)is_prime[i]=1;
+
+    for(ll i=3; i<=sq; i+=2)
+    {
+        if(is_prime[i]==1)
+        {
+            for(ll j=i*i; j<=n; j+=(i+i))
+                is_prime[j]=0;
+        }
+    }
+    primes.PB(2);
+    is_prime[2]=1;
+    for(ll i=3; i<=n; i+=2)
+    {
+        if(is_prime[i]==1)primes.PB(i);
+    }
+}
+ll NOD(ll n)
+{
+    ll ans=1;
+    for(auto u : primes)
+    {
+        ll cnt=0;
+        if(u*u > n)break;
+        if(n%u==0)
+        {
+            while(n%u==0)
+            {
+                n/=u;
+                cnt++;
+            }
+            cnt++;
+            ans*=cnt;
+        }
+    }
+    if(n>1) ans*=2;
+    return ans;
+}
+int main()
+{
+    optimize();
+    ll limt=1e6+123;
+    sieve(limt);
+    ll test;
+    cin>>test;
+    while(test--)
+    {
+        ll n;
+        cin>>n;
+        cout<<NOD(n)<<endl;
+    }
+}
+
+//Alhamdulillah...
+
+
+
+
+
+
